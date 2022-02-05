184692f284cd1b8a0fd3ce05ffc6d3782e23a92b
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-02-05 20:30:50 +0530

    Update Python Codes.py

--- Python Codes.py
+++ Python Codes.py
@@ -1,1 +1,30 @@
+# Fibonacci Series till the limit.
 
+n = int(input("enter the limit of series: "))
+print("The Fibanacci series is ")
+a=0
+b=1
+print(a)
+print(b)
+sum=0
+while(sum<n-2):
+    sum=a+b
+    print(sum)
+    a=b
+    b=sum
+    
+    
+    
+# Fibonacci Series of n number of terms.
+
+nterms = int(input("Number of many terms: "))
+
+n1, n2 = 0, 1
+count = 0
+
+while count < nterms:
+  print(n1)
+  nth = n1 + n2
+  n1 = n2
+  n2 = nth
+  count += 1
